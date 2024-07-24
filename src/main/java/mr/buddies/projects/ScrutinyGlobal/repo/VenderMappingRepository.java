804a5c675b3b9ccddb7acb54423f10cd32ea44ba
pushpak1308 <167736667+pushpak1308@users.noreply.github.com>
2024-07-25 01:22:52 +0530

    query changes  for assign vendor on project table

--- src/main/java/mr/buddies/projects/ScrutinyGlobal/repo/VenderMappingRepository.java
+++ src/main/java/mr/buddies/projects/ScrutinyGlobal/repo/VenderMappingRepository.java
@@ -13,7 +13,7 @@ import mr.buddies.projects.ScrutinyGlobal.model.VenderMappingDetails;
 @Repository
 public interface VenderMappingRepository extends JpaRepository<VenderMappingDetails,Integer> {
 
-	@Query(value = "SELECT * FROM vender_mapping_details vmd"
-	 		+ " join vender_details vd  on vmd.vender_id=vd.vender_id join register_user ru on vmd.vender_id=ru.user_id where vender_mapping_id=:venderMappingId ", nativeQuery = true)
+	@Query(value = "SELECT vmd.vendor_id,vmd.quota_fullurl,vmd.successurl,vmd.terminateurl,ru.name,vmd.vender_mapping_id,vmd.rate  FROM vender_mapping_details vmd"
+	 		+ " join vender_details vd  on vmd.vendor_id=vd.vender_id join register_user ru on vmd.vendor_id=ru.user_id where vender_mapping_id=:venderMappingId ", nativeQuery = true)
 	 	public List<Map<String,Object>> getProjectVenderList(@Param("venderMappingId") String venderMappingId);
 }
